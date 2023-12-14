e280eb65390e20a3f0de73f706b0749de14d8195
zzzdks760 <zzzdks760@naver.com>
2023-12-14 10:14:30 +0900

    html초기 화면

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <button>AC</button>
+          <button>%</button>
+          <button>/</button>
+          <button>&lt;</button>
+        </div>
+
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>+</button>
+        </div>
+
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+        </div>
+
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>X</button>
+        </div>
+
+        <div>
+          <button></button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
